76e876ec99431616477b42d4434dcf06a68367d7
Martin Agoha <martin4dtruth@gmail.com>
2023-09-20 05:56:10 +0100

    updated all

--- app.py
+++ app.py
@@ -1,11 +1,44 @@
-from flask import Flask, render_template
-from models import db, User, Group, UserGroup, Event, GroupEvent, InterestedEvent, Comment, Image, init_db
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+from flask import Flask, render_template, redirect, url_for, request, flash
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email  # Import Email validator
+from werkzeug.security import check_password_hash
+
+# Import the Event class from models
+from models import db, User, Event, init_db
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///event_app.db'
+app.secret_key = 'ddb70a322429109f2172612d4264d37e5d6e5844c4ee77b2'  # Replace with a secure random key
 
 # Initialize the database
-init_db(app)
+db.init_app(app)
+
+# Initialize Flask-Login
+login_manager = LoginManager()
+login_manager.login_view = 'login'
+login_manager.init_app(app)
+
+# Create a login form
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
+
+# Create a registration form
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Register')
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 # Routes
 @app.route('/')
@@ -17,6 +50,49 @@ def index():
 def event_details(event_id):
     event = Event.query.get(event_id)
     return render_template('event_details.html', event=event)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+
+        user = User.query.filter_by(username=username).first()
+        if user and check_password_hash(user.password, password):
+            login_user(user)
+            flash('Logged in successfully!', 'success')
+            return redirect(url_for('index'))
+        else:
+            flash('Login unsuccessful. Please check your credentials.', 'danger')
+
+    return render_template('login.html', form=form)
+
+# Add a route for registration
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        # Create a new user
+        username = form.username.data
+        email = form.email.data  # Get the email field
+        password = form.password.data
+
+        user = User(username=username, email=email, password=password)  # Include email in user creation
+        db.session.add(user)
+        db.session.commit()
+
+        flash('Registration successful! You can now log in.', 'success')
+        return redirect(url_for('login'))
+
+    return render_template('register.html', form=form)
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('Logged out successfully!', 'success')
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True)
